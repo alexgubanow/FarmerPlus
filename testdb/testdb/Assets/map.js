e1fe4d0c64c618c03b1ba8ea41630f4768e1137b
Oleksandr Hubanov <alex@0x0h.com>
2019-06-01 18:29:03 +0300

    Add files via upload

---
+++ testdb/testdb/Assets/map.js
@@ -0,0 +1,133 @@
+﻿var map, light, old_val, selid, curre, currbaselayer;
+var areas = new L.featureGroup();
+function init() {
+    var HYBRID = L.tileLayer('https://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}', {
+        maxZoom: 26,
+        subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+    });
+    var Google = L.tileLayer('https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+        maxZoom: 26,
+        subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+    });
+    map = L.map('map', {
+        //fadeAnimation: false,
+        editable: true,
+        center: [50.0031533, 36.3025844],
+        zoom: 16,
+        layers: [HYBRID, areas]
+    });
+    var baseLayers = {
+        "Спутник": HYBRID,
+        "Google": Google
+    };
+    var overlays = {
+        "Паи": areas
+    };
+    L.control.layers(baseLayers, overlays).addTo(map);
+    map.on('baselayerchange', mapbaselayerchange);
+    L.control.scale({ imperial: false }).addTo(map);
+    //HYBRID.on("load", function () { myGlobalObject.loadfin() });
+    //var lat = (((sessionStorage.getItem('mapcoords')).replace("LatLng(", "")).replace(")", "")).split(',')[0];
+    //var lng = (((sessionStorage.getItem('mapcoords')).replace("LatLng(", "")).replace(")", "")).split(',')[1];
+    //map.setView([parseFloat(lat), parseFloat(lng)], parseFloat(sessionStorage.getItem('mapzoom')));
+    try {
+        
+        var lat = (((localStorage.getItem('mapcoords')).replace("LatLng(", "")).replace(")", "")).split(',')[0];
+        var lng = (((localStorage.getItem('mapcoords')).replace("LatLng(", "")).replace(")", "")).split(',')[1];
+        map.setView([parseFloat(lat), parseFloat(lng)], parseFloat(localStorage.getItem('mapzoom')));
+
+    } catch (err) {
+    }
+}
+function savemapstate()
+{
+    //sessionStorage.setItem('mapcoords', map.getCenter());
+    //sessionStorage.setItem('mapzoom', map.getZoom());
+    localStorage.setItem('mapcoords', map.getCenter());
+    localStorage.setItem('mapzoom', map.getZoom());
+}
+
+function lights(json, pan) {
+    if (map.hasLayer(light)) { map.removeLayer(light) }
+    var coords = JSON.parse(json);
+    try
+    {
+        if (currbaselayer.name == "Спутник") {
+            light = L.polyline(coords, { color: 'Yellow', weight: '2' }).addTo(map);
+        }
+        else {
+            light = L.polyline(coords, { color: 'red', weight: '2' }).addTo(map);
+        }
+    }
+    catch(ex)
+    { light = L.polyline(coords, { color: 'Yellow', weight: '2' }).addTo(map); }
+
+    if (pan) {
+        map.fitBounds(light.getBounds());
+    }
+}
+function mapbaselayerchange(e) {
+    currbaselayer = e;
+    if (map.hasLayer(light)) {
+        var coords = light.getLatLngs();
+        map.removeLayer(light);
+        if (e.name == "Спутник") {
+            light = L.polyline(coords, { color: 'Yellow', weight: '2' }).addTo(map);
+        }
+        else {
+            light = L.polyline(coords, { color: 'red', weight: '2' }).addTo(map);
+        }
+    }
+}
+function loadarea(str, descript) {
+    var lay = L.geoJson(JSON.parse(str), {
+        style: function (feature) {            
+            return { color: feature.properties.color, weight: feature.properties.weight };
+        }
+    }).bindPopup(descript);
+    lay.on('click', onClick);
+    areas.addLayer(lay);
+}
+function updatefdb() {
+    var prevpopup = document.getElementsByClassName("leaflet-popup-content")[0].innerHTML;
+    var tempDiv = document.createElement('div');
+    tempDiv.innerHTML = prevpopup;
+    tempDiv.getElementsByTagName("input")[0].defaultValue = document.getElementById("area_input").value;
+    curre.layer.bindPopup(tempDiv.innerHTML);
+    myGlobalObject.update([document.getElementById("index").innerText, document.getElementById("area_input").value]);
+    //document.getElementById("savebtn").style.visibility = "hidden";
+}
+//function enblbutton() {
+//    if (old_val != document.getElementById("area_input").value) {
+//        document.getElementById("savebtn").style.visibility = "visible";
+//        old_val = document.getElementById("area_input").value;
+//    }
+//    else {
+//        document.getElementById("savebtn").style.visibility = "hidden";
+//    }
+//}
+function pan_to(json) {
+    if (map.hasLayer(light)) { map.removeLayer(light) }
+    var coords = JSON.parse(json);
+    coords.push(coords[0]);
+    try {
+        if (currbaselayer.name == "Спутник") {
+            light = L.polyline(coords, { color: 'Yellow', weight: '3' }).addTo(map);
+        }
+        else {
+            light = L.polyline(coords, { color: 'red', weight: '3' }).addTo(map);
+        }
+    }
+    catch (ex)
+    { light = L.polyline(coords, { color: 'Yellow', weight: '3' }).addTo(map); }
+    map.addLayer(light);
+    map.setView(coords[0], 15);
+}
+function onClick(e) {
+    curre = e;
+    e.layer.openPopup(e.latlng);
+    old_val = document.getElementById("area_input").value;
+}
+function removeLayer() {
+    if (map.hasLayer(light)) { map.removeLayer(light) }
+}
---
+++ testdb/testdb/MainActivity.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using MySql.Data.MySqlClient;
+using Android.Webkit;
+
+namespace testdb
+{
+    [Activity(Label = "testdb", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        WebView web_view;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+            web_view = FindViewById<WebView>(Resource.Id.webview);
+            web_view.Settings.JavaScriptEnabled = true;
+            web_view.SetWebViewClient(new HelloWebViewClient());
+            web_view.LoadUrl("file:///android_asset/index.html");
+
+            string strProvider = "Data Source=159.224.55.85; Database=c4barabandb;User ID=c4c4;Password=sad15forever;";
+            MySqlConnection cnt = new MySqlConnection(strProvider);
+            try
+            {
+                cnt.Open();
+                MySqlCommand cmd = new MySqlCommand("select version();", cnt);
+                //cmd.ExecuteNonQuery();
+                String result = cmd.ExecuteScalar().ToString();
+                EditText edtext = FindViewById<EditText>(Resource.Id.editText1);
+                edtext.Text = result;
+                cnt.Close();
+            }
+            //try
+            //{
+            //    MySqlConnection sqlconn;
+            //    string connsqlstring = string.Format("Server=159.224.55.85;Port=3306;database=c4barabandb;User Id=c4c4;Password=sad15forever;charset=utf8");
+            //    sqlconn = new MySqlConnection(connsqlstring);
+            //    sqlconn.Open();
+            //    string queryString = "select version();";
+            //    MySqlCommand sqlcmd = new MySqlCommand(queryString, sqlconn);
+            //    String result = sqlcmd.ExecuteScalar().ToString();
+            //    EditText edtext = FindViewById<EditText>(Resource.Id.editText1);
+            //    edtext.Text = result;
+            //    sqlconn.Close();
+            //}
+            catch (Exception ex)
+            {
+                EditText edtext = FindViewById<EditText>(Resource.Id.editText1);
+                edtext.Text = ex.Message;
+            }
+        }
+        public class HelloWebViewClient : WebViewClient
+        {
+            public override bool ShouldOverrideUrlLoading(WebView view, string url)
+            {
+                view.LoadUrl(url);
+                return true;
+            }
+        }
+    }
+}
+
